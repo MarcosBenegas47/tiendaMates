5e6fd9b4323746d8891cf2e74570eeeec8a5c6d8
MarcosBenegas47 <mbenegas@frba.utn.edu.ar>
2024-10-18 19:10:05 -0300

    agregando imagenes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import styles from "../resources/styles/pageMain.module.css";
+import "normalize.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
