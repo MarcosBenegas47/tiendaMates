1a7c1a6250203341343562a70f0a29d1f48129f5
MarcosBenegas47 <marcobenega@gmail.com>
2024-10-18 14:12:09 -0300

    imagenes

--- src/app/api/images/[id]/route.ts
+++ src/app/api/images/[id]/route.ts
@@ -1,13 +1,20 @@
 import { NextResponse } from 'next/server';
 import { join } from 'path';
-import { promises as fs } from 'fs';
+import { promises as fs, readdirSync } from 'fs';
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     const { id } = params;
     // Ruta a la imagen en tu directorio público
-    const ruta = join(process.cwd(), 'src', 'resources','images', `${id}.jpg`);
+    const ruta = join(process.cwd(), 'src', 'resources','images');
+    const file = readdirSync(ruta);
+    const fileFind = file.find(file => file.startsWith(`${id}.`));
+    var pathFile:string ="";
+    if(fileFind){
+      pathFile = join(ruta, fileFind);
+
+    }
   try {
-    const imagen = await fs.readFile(ruta)
+    const imagen = await fs.readFile(pathFile)
     return new NextResponse(imagen)
   } catch (error) {
     return new NextResponse('Image not Found',{status:404})
--- src/app/component/Product.tsx
+++ src/app/component/Product.tsx
@@ -3,18 +3,18 @@ import { Productos } from "@/Productos";
 import style from "../../resources/styles/product.module.css"
 import Image from "next/image";
 
-import img from "./im.jpg"
+import img from "../../resources/images/10.png"
 
 export default async function Product({products}:{products:Productos[]} ){
-const img = ["/im.jpg"]
     return(<>
         <ul className={style.productList}>
             {products?.map(({id,codigo,cantidad,descripcion,p_Unitario_final} )=>(
                 cantidad >0 && (
                 <li className={style.product} key={id}>
-                    <Image src={`/api/images/${id}`} alt="algun mate im" width={300} height={250} className={style.imagen}/>
+                    <Image src={`/api/images/${id}`} alt="algun mate im" width={300} height={290} className={style.imagen}/>
                     <div className={style.textCard}>
                         <h3>{descripcion}</h3>
+                        {id}
                         <strong className={style.precio}>${p_Unitario_final}</strong>
                     </div>
                     
