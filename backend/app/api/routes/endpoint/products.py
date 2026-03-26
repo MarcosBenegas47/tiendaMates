2f383a7a0e007d94e3a9f8da9b35f4ae6d09e8a1
MarcosBenegas47 <mbenegas@frba.utn.edu.ar>
2026-03-26 18:32:21 -0300

    me olvide desubir el endopoint

--- backend/app/api/routes/endpoint/products.py
+++ backend/app/api/routes/endpoint/products.py
@@ -5,6 +5,9 @@ from app.db.database import getDataBase, getDbAdmin
 from app.repositories.product_repositoriy import ProducRepository
 
 from app.schemas.product import ProductCreate
+import json
+
+from typing import Optional 
 
 router = APIRouter( prefix="/store", tags=["Productos"])
 responseProduct = ProducRepository()
@@ -24,6 +27,14 @@ def searchProduct(queryLink:str,db:Session = Depends(getDataBase)):
 @router.get("/categorys")
 def getCategory(db:Session = Depends(getDataBase)):
     return responseProduct.getCategory(db)
+
+@router.get("/categorys/products")
+def getproducts(db:Session = Depends(getDataBase), id:Optional[str] = None,limit:int = 6 ,offset:int=0):
+    ids_list= None
+    if id:
+        ids_list = json.loads(id)  
+    return responseProduct.get_all(db, id = ids_list ,limit = limit, offset = offset)
+
 @router.get("/destacados")
 def getDestacados(db:Session = Depends(getDataBase)):
     return responseProduct.getDestacados(db)
