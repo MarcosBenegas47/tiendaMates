89f71e1e8a534db2bd5a899046d5ed47a5af30df
MarcosBenegas47 <mbenegas@frba.utn.edu.ar>
2026-08-26 12:15:18 -0300

    update main cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from app.api.routes.router import apiRouter 
 from fastapi.middleware.cors import CORSMiddleware
 import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 ALLOWED_ORIGINS= os.getenv("ALLOWED_ORIGINS")
 
 app = FastAPI()
 
-
+ALLOWED_ORIGINS = json.loads(ALLOWED_ORIGINS)
 
 app.add_middleware(
     CORSMiddleware,
